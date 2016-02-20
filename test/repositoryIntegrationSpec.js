b71cacce1576db8ffe7627fead94b109864b05f9
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-02-20 21:57:29 +0000

    Refactor

--- test/repositoryIntegrationSpec.js
+++ test/repositoryIntegrationSpec.js
@@ -2,6 +2,7 @@
 
 let should = require('chai').should();
 let mongo = require('mongodb');
+
 class MongoRepository {
   constructor(connection) {
     this._connection = connection;
@@ -10,9 +11,12 @@ class MongoRepository {
   getPost(id, callback) {
     let ObjectId = new mongo.ObjectID(id);
     mongo.connect(this._connection, (err, db) => {
-
         db.collection('posts').findOne({"_id": ObjectId}, (err, post) => {
-          callback(post);
+          callback({
+            'title': post.title,
+            'content': post.content,
+            'author': post.author
+          });
         });
       });
     }
@@ -23,15 +27,13 @@ describe('post repository should', () => {
     let repository = new MongoRepository("mongodb://localhost:12345/blog");
     let postId = '56c8d54262a98d965a285f00';
     let postDTO = {
-      '_id' : '56c8d54262a98d965a285f00',
       'title' : 'Foo Bar Title',
       'content' : 'Foo Bar Content',
       'author' : 'Foo Bar Author'
     };
     repository.getPost(postId, (post) => {
-      JSON.stringify(post).should.be.equal(JSON.stringify(postDTO));
+      post.should.be.deep.equal(postDTO);
       done();
     });
-
   });
 });
