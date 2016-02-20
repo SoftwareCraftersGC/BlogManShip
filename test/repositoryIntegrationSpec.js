a60f0e1f563ecd7a5ed998951fc5283521ff5e09
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-02-20 22:13:45 +0000

    Refactor

--- test/repositoryIntegrationSpec.js
+++ test/repositoryIntegrationSpec.js
@@ -12,17 +12,20 @@ class MongoRepository {
     let ObjectId = new mongo.ObjectID(id);
     mongo.connect(this._connection, (err, db) => {
         db.collection('posts').findOne({"_id": ObjectId}, (err, post) => {
-          if (!post) {
-            callback({});
-          } else {
-            callback({
-              'title': post.title,
-              'content': post.content,
-              'author': post.author
-            });
-        }
+          callback(this._exportPost(post));
         });
       });
+    }
+
+    _exportPost (post) {
+      if (!post) {
+        return {};
+      }
+      return {
+        'title': post.title,
+        'content': post.content,
+        'author': post.author
+      }
     }
 }
 
