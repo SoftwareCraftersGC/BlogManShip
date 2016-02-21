2199c1ff0323c642d09592760dca184b1f5004ac
Rubén Díaz Martínez <rubendm92@gmail.com>
2016-02-21 11:24:55 +0000

    Add eslint to check lint javascript

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,15 @@
 
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const eslint = require('gulp-eslint');
 
-gulp.task('test', () => {
+gulp.task('lint', () => {
+    return gulp.src(['**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], () => {
   return gulp.src(['test/*.js'], {read: false}).pipe(mocha({reporter: 'spec'}));
 });
