db91f9b8ed8791076a902d3566e2d2d856047291
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-02-21 00:42:49 +0000

    Refactor tests: now post is created and deleted when test is executed

--- test/repositoryIntegrationSpec.js
+++ test/repositoryIntegrationSpec.js
@@ -1,25 +1,43 @@
 'use strict';
 
 let should = require('chai').should();
+let mongo = require('mongodb');
 
 let MongoRepository = require('../src/infrastructure/mongoRepository');
 
 describe('post repository should', () => {
-  let fakeMongoUrl;
+  const fakeMongoUrl = 'mongodb://localhost:12345/blog';
+  const postId = '56c8d54262a98d965a285f00';
+  const postDTO = {
+    'title' : 'Foo Bar Title',
+    'content' : 'Foo Bar Content',
+    'author' : 'Foo Bar Author'
+  };
+
   let repository;
 
+  before((done) => {
+    mongo.connect(fakeMongoUrl, (err, db) => {
+        db.collection('posts').insert({
+          '_id' : postId,
+          'title' : postDTO.title,
+          'content' : postDTO.content,
+          'author' : postDTO.author
+        }, done);
+    });
+  });
+
+  after((done) => {
+    mongo.connect(fakeMongoUrl, (err, db) => {
+        db.collection('posts').remove({'_id' : postId}, done);
+    });
+  })
+
   beforeEach(() => {
-    fakeMongoUrl = 'mongodb://localhost:12345/blog';
     repository = new MongoRepository(fakeMongoUrl);
   })
 
   it('return a specific post when getPost is called', (done) => {
-    let postId = '56c8d54262a98d965a285f00';
-    let postDTO = {
-      'title' : 'Foo Bar Title',
-      'content' : 'Foo Bar Content',
-      'author' : 'Foo Bar Author'
-    };
     repository.getPost(postId).then((post) => {
         post.should.be.deep.equal(postDTO)
         done();
