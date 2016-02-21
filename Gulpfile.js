31b6191aff6c438dbc1e77b767ff6b624edd1f05
Rubén Díaz Martínez <rubendm92@gmail.com>
2016-02-21 11:08:07 +0000

    Run tests using gulp

---
+++ Gulpfile.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+
+gulp.task('test', () => {
+  return gulp.src(['test/*.js'], {read: false}).pipe(mocha({reporter: 'spec'}));
+});
