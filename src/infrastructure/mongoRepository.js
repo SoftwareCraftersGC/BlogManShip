e8283749557498c4bbbd02049cf52f05b36933c7
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-04-06 21:04:34 +0100

    Ignore

--- src/infrastructure/mongoRepository.js
+++ src/infrastructure/mongoRepository.js
@@ -4,65 +4,65 @@ let mongo = require('mongodb');
 
 function MongoRepository(uri) {
 
-  function executeOnDatabase(callback) {
-    mongo.connect(uri, callback);
-  }
-
-  function createPost(post) {
-    return new Promise((resolve) => {
-      const insertPost = (err, db) => {
-        db.collection('posts').insert(post, () => {
-          db.close();
-          resolve();
-        });
-      };
-      executeOnDatabase(insertPost);
-    });
-  }
-
-  function getAllPosts() {
-    return new Promise((resolve) => {
-      const findAllPosts = (err, db) => {
-        db.collection('posts').find((err, cursor) => {
-          cursor.toArray((err, posts) => {
-            db.close();
-            resolve(posts);
-          });
-        });
-      };
-      executeOnDatabase(findAllPosts);
-    });
-  }
-
-  function getPost(id) {
-    let ObjectId = new mongo.ObjectID(id);
-    return new Promise((resolve) => {
-      const findPost = (err, db) => {
-        db.collection('posts').findOne({'_id': ObjectId}, (err, post) => {
-          db.close();
-          resolve(exportPost(post));
-        });
-      };
-      executeOnDatabase(findPost);
-    });
-  }
-
-  function exportPost (post) {
-    if (!post) {
-      return {};
+    function executeOnDatabase(callback) {
+        mongo.connect(uri, callback);
     }
-    return {
-      'title': post.title,
-      'content': post.content,
-      'author': post.author
-    };
-  }
 
-  return {
-    createPost : createPost,
-    getAllPosts: getAllPosts,
-    getPost: getPost
-};
+    function createPost(post) {
+        return new Promise((resolve) => {
+            const insertPost = (err, db) => {
+                db.collection('posts').insert(post, () => {
+                    db.close();
+                    resolve();
+                });
+            };
+            executeOnDatabase(insertPost);
+        });
+    }
+
+    function getAllPosts() {
+        return new Promise((resolve) => {
+            const findAllPosts = (err, db) => {
+                db.collection('posts').find((err, cursor) => {
+                    cursor.toArray((err, posts) => {
+                        db.close();
+                        resolve(posts);
+                    });
+                });
+            };
+            executeOnDatabase(findAllPosts);
+        });
+    }
+
+    function getPost(id) {
+        let ObjectId = new mongo.ObjectID(id);
+        return new Promise((resolve) => {
+            const findPost = (err, db) => {
+                db.collection('posts').findOne({'_id': ObjectId}, (err, post) => {
+                    db.close();
+                    resolve(exportPost(post));
+                });
+            };
+            executeOnDatabase(findPost);
+        });
+    }
+
+    function exportPost (post) {
+        if (!post) {
+            return {};
+        }
+        return {
+            'title': post.title,
+            'content': post.content,
+            'author': post.author
+        };
+    }
+
+    return {
+        createPost : createPost,
+        getAllPosts: getAllPosts,
+        getPost: getPost
+    };
 }
 
 module.exports = MongoRepository;
