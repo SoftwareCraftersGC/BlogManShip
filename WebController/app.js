c626151873d4d38046ec9b9d73ba8868d2cb4f10
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-04-08 18:27:30 +0100

    Serve static files

--- WebController/app.js
+++ WebController/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+app.use('/', express.static(__dirname + '/../Web'));
+
 const repository = require('../src/infrastructure/mongoRepository')('mongodb://localhost:12345/blog');
 
 const getAllPostsAction = require('../src/actions/getallpostsaction')(repository);
@@ -13,6 +15,5 @@ const createPostAction = require('../src/actions/createpostaction')(repository);
 app.get('/posts', (req, res) => getAllPostsAction.execute().then((posts) => res.send(posts)));
 app.get('/posts/:id', (req, res) => getPostAction.execute(req.params.id).then((post) => res.send(post)));
 app.post('/posts', (req, res) => createPostAction.execute(req.body.post).then((result) => res.send(result)));
-
 
 app.listen(5000, () => console.log('Running server'));
