6ff8b9fcf76196b1e4fe4f743d220775d7d6b899
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-04-13 19:39:22 +0100

    Refactor: posts are sorted by last added date

--- Business/Tests/actionsSpec.js
+++ Business/Tests/actionsSpec.js
@@ -2,6 +2,8 @@
 
 let sinon = require('sinon');
 
+require('chai').should();
+
 let GetAllPostsAction = require('../src/actions/getallpostsaction');
 let CreatePostAction = require('../src/actions/createpostaction');
 let GetPostAction = require('../src/actions/getpostaction');
@@ -10,17 +12,37 @@ let GetLastPostsAction = require('../src/actions/GetLastPostsAction');
 describe('getAllPostAction should return', () => {
     it('a list with all the posts available', (done) => {
 
+        const postOneDate = new Date().getTime();
+        const postTwoDate = new Date().getTime()  + 1 * 24 * 3600 * 1000;
+
+        const expectedPosts = [
+            {
+                title : 'AnyTitle',
+                content : 'AnyContent',
+                author : 'AnyAuthor',
+                date : postOneDate
+            },
+            {
+                title : 'AnyTitle',
+                content : 'AnyContent',
+                author : 'AnyAuthor',
+                date : postTwoDate
+            }
+        ];
+
         let getAllPosts = sinon.spy();
         let postRepository = {
             getAllPosts : () => new Promise((resolve) => {
                 getAllPosts();
-                resolve();
+                resolve(expectedPosts.slice(0));
             })
         };
 
         let action = GetAllPostsAction(postRepository);
-        action.execute().then(() => {
+        action.execute().then((posts) => {
             sinon.assert.calledOnce(getAllPosts);
+            posts[0].should.be.deep.equal(expectedPosts[1]);
+            posts[1].should.be.deep.equal(expectedPosts[0]);
             done();
         });
     });
--- Business/src/actions/getallpostsaction.js
+++ Business/src/actions/getallpostsaction.js
@@ -4,7 +4,9 @@ function GetAllPostsAction(repository) {
 
   return {
     execute : () => new Promise((resolve, reject) => {
-        repository.getAllPosts().then((posts) =>resolve(posts))
+        repository.getAllPosts().then((posts) => {
+            resolve(posts.sort((post1, post2) => post1.date < post2.date));
+        })
         .catch((err) => reject(err));
     })
   };
